c09b37188db465455a35ce54ef6734fc33b01f94
nkprasanna <nkprasasnna@gmail.com>
2016-03-02 12:35:14 +0530

    Console Testing

--- ForGetTheMilk/Controllers/TaskController.cs
+++ ForGetTheMilk/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,8 +11,38 @@ namespace ForGetTheMilk.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(Tasks);
         }
+        public static readonly List<Task> Tasks = new List<Task>();
+         
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+            var taskItem = new Task(task);
+            Tasks.Add(taskItem);
+            return RedirectToAction("Index");
+        }
+       
+     public class Task
+     {
+         public Task(string task)
+         {
+             Description = task;
+             var dueDatePattern = new Regex(@"may\s(\d)");
+             //task = task.ToLower();
+             bool hasDueDate = dueDatePattern.IsMatch(task);
+             if (hasDueDate)
+             {
+                
+                 var dueDtae = dueDatePattern.Match(task);
+                 var day = Convert.ToInt32(dueDtae.Groups[1].Value);
+                 
+                 DueDate = new DateTime(DateTime.Today.Year, 5, day);
+             }
+         }
+         public string  Description { get; set; }
+         public DateTime? DueDate { get; set; }
 
+     }
     }
 }
---
+++ GetMilkVerification/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace GetMilkVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("\nAdd a task:\n");
+                var input = Console.ReadLine();
+                var task = new ForGetTheMilk.Controllers.TaskController.Task(input);
+
+                Console.WriteLine("Description: "+ task.Description);
+                Console.WriteLine("Due Date: "+ task.DueDate);
+            }
+        }
+    }
+}
